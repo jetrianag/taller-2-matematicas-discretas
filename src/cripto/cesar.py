ae9deca7629fd326dfe4f512854cd8a96e4e033a
Diego <dirodriguezsa@unal.edu.co>
2026-07-20 20:49:13 -0500

    Implemente el cifrado y el descifrado Cesar

---
+++ src/cripto/cesar.py
@@ -0,0 +1,15 @@
+alfabeto = 26
+def desplazar_caracter(caracter: str, k: int) -> str:
+    if caracter.isalpha():
+        base = ord('A') if caracter.isupper() else ord('a')
+        posicion = (ord(caracter) - base 
+        nueva_posicion = (posicion + k) % alfabeto
+        return chr(base + nueva_posicion)  
+    return caracter
+
+    def cifrar_cesar(texto: str, k: int) -> str:
+        k = k % alfabeto
+        return ''.join(desplazar_caracter(c, k) for c in texto)
+
+    def descifrar_cesar(texto: str, k: int) -> str:
+        return cifrar_cesar(texto, -k)
