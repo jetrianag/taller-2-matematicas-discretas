be49892dbfdcd74048a1b1e74a0406ceb78a7754
Diego <dirodriguezsa@unal.edu.co>
2026-07-20 20:54:32 -0500

    cree fuerza bruta cesar que es basicamente si no se que k uso la persona que me mando el mensaje, los prueba todos y ahí uno mira cual mensaje tiene sentido. Tambien arregle un parentesis que habia antes que no tenia sentido.

--- src/cripto/cesar.py
+++ src/cripto/cesar.py
@@ -2,7 +2,7 @@ alfabeto = 26
 def desplazar_caracter(caracter: str, k: int) -> str:
     if caracter.isalpha():
         base = ord('A') if caracter.isupper() else ord('a')
-        posicion = (ord(caracter) - base 
+        posicion = ord(caracter) - base 
         nueva_posicion = (posicion + k) % alfabeto
         return chr(base + nueva_posicion)  
     return caracter
@@ -13,3 +13,6 @@ def desplazar_caracter(caracter: str, k: int) -> str:
 
     def descifrar_cesar(texto: str, k: int) -> str:
         return cifrar_cesar(texto, -k)
+    
+    def fuerza_bruta_cesar(texto: str) -> dict:
+        return {k: descifrar_cesar(texto, k) for k in range(alfabeto)}
