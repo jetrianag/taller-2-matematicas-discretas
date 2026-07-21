1562b89b6d02768f56eea9315c7b65d69e8d8318
Diego <dirodriguezsa@unal.edu.co>
2026-07-20 21:34:10 -0500

    Verifica el ejemplo del taller

--- tests/cesar_test.py
+++ tests/cesar_test.py
@@ -5,3 +5,10 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 from cripto.cesar import cifrar_cesar, descifrar_cesar, fuerza_bruta_cesar
 
+class TestCesar(unittest.TestCase):
+
+    def test_cifrado_ejemplo_taller(self):
+        #caso minimo que estaba en el taller
+        self.assertEqual(cifrar_cesar("HOLA UNAL", 3), "KROD XQDO")
+
+    
