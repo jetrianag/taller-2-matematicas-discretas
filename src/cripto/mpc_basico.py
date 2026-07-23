2d6e2de8f7c1907ff8c833966c616f326b00989f
Diego <dirodriguezsa@unal.edu.co>
2026-07-23 10:41:26 -0500

    se agregan 3 funciones, una para sumar inicialmente las notas que tiene cada servidor, luego otra que suma el resultado de cada servidor y otra que calcula el promedio de la suma total de los servidores

--- src/cripto/mpc_basico.py
+++ src/cripto/mpc_basico.py
@@ -17,3 +17,12 @@ def repartir_notas(notas: list, M: int = MODULO_POR_DEFECTO) -> tuple:
         partes_servidor2.append(s2)
         partes_servidor3.append(s3)
     return partes_servidor1, partes_servidor2, partes_servidor3
+
+def suma_parcial_servidor(partes_de_un_servidor: list, M: int = MODULO_POR_DEFECTO) -> int:
+    return sum(partes_de_un_servidor) % M
+def reconstruir_suma(suma1: int, suma2: int, suma3: int, M: int = MODULO_POR_DEFECTO) -> int:
+    return (suma1 + suma2 + suma3) % M
+def calcular_promedio(suma_total: int, cantidad_notas: int) -> float:
+    if cantidad_notas == 0:
+        raise ValueError("No hay notas para calcular el promedio.")
+    return suma_total / cantidad_notas
