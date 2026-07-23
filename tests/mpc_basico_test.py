105abcbeb827c813fbe48c2782e07a68adf1a0c6
Diego <dirodriguezsa@unal.edu.co>
2026-07-23 10:54:16 -0500

    el tercer test verifica que ningun servidor vea la nota

--- tests/mpc_basico_test.py
+++ tests/mpc_basico_test.py
@@ -17,3 +17,9 @@ class TestMPC(unittest.TestCase):
         resultado = simular_protocolo([40, 35, 50, 25])
         self.assertEqual(resultado["suma_total"], 150)
         self.assertEqual(resultado["promedio"], 37.5)
+
+     def test_ningun_servidor_ve_la_nota_original(self):
+        M = 1000003
+        nota = 45
+        s1, s2, s3 = generar_partes(nota, M)
+        self.assertTrue(s1 != nota or s2 != nota or s3 != nota)
