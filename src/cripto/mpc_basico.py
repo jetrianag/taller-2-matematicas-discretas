f1ead94c6b86797735bcd40b9d21f819737dfd71
Diego <dirodriguezsa@unal.edu.co>
2026-07-23 10:31:06 -0500

    se agrego el reparto de noras entre los 3 servidores.

--- src/cripto/mpc_basico.py
+++ src/cripto/mpc_basico.py
@@ -8,3 +8,12 @@ def generar_partes(nota: int, M: int = MODULO_POR_DEFECTO) -> tuple:
     s2 = random.randint(0, M - 1)
     s3 = (nota - s1 - s2) % M
     return s1, s2, s3
+
+def repartir_notas(notas: list, M: int = MODULO_POR_DEFECTO) -> tuple:
+    partes_servidor1, partes_servidor2, partes_servidor3 = [], [], []
+    for nota in notas:
+        s1, s2, s3 = generar_partes(nota, M)
+        partes_servidor1.append(s1)
+        partes_servidor2.append(s2)
+        partes_servidor3.append(s3)
+    return partes_servidor1, partes_servidor2, partes_servidor3
