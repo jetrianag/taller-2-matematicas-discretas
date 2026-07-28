725255dcc140ca62f131eb1b3b2fd9fad5ba414b
Diego <dirodriguezsa@unal.edu.co>
2026-07-27 19:46:13 -0500

    agregue otra funcion que mas o menos funciona como verificador de si es valido el coloreo o no

--- src/grafos/coloreo_grafos.py
+++ src/grafos/coloreo_grafos.py
@@ -7,3 +7,9 @@ def colorear_grafo(grafo: dict) -> dict:
             color += 1
         colores[vertice] = color
     return colores
+def es_coloreo_valido(grafo: dict, colores: dict) -> bool:
+    for vertice, vecinos in grafo.items():
+        for vecino in vecinos:
+            if colores[vertice] == colores[vecino]:
+                return False
+    return True
